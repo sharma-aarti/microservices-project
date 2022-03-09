aa0421e90e3156cc974fa697c9570882c3210701
sharma-aarti <sharmaaarti50528@gmail.com>
2022-03-09 11:58:21 +0530

    Initial commit

---
+++ src/main/java/com/microservicesDemo/movieinfoservice/models/Movie.java
@@ -0,0 +1,27 @@
+package com.microservicesDemo.movieinfoservice.models;
+
+public class Movie {
+ private String movieId;
+ private String Name;
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        Name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
